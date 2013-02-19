51a201ae571890b86ce62c82168be7cd4ee8cde4
user <user@users-MacBook-Pro.local>
2013-02-19 16:07:34 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BcslotsRails::Application.config.secret_token = '18f435587c73b17016b361f483bb8e17ea717d0d5604927c2b37f690b008c790ae02c6e688cb4f3ca14d8ede420e7d209084483bef631173c2c3a42eec72580b'
