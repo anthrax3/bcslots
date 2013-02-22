bb4997bd45a05f79a7375012b9f42dbc8b339fea
user <user@users-MacBook-Pro.local>
2013-02-22 00:48:17 -0500

    before state machine

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,36 +1,87 @@
 class HomeController < ApplicationController
   def index
-    #four cases:
-    # 1) has no uuid or address
-    # 2) has a uuid, but no address
-    # 3) has a uuid and address
-    # 4) has no uuid, but an address
-    # ignore 4 for now
-    # case 2 happens in between job queue enter and completion
-    # if refresh, should make a new job and ignore last address, spamming server more? I say yes
-    # , to be a little simpler. Also, if redis is set for fsync/per second, a case could occur
-    # where the user has a uuid, but no address is saved. 
+
     r = Redis.new
     c = cookies[:uuid]
-    if (c.nil? || r.get(c).nil?)
-      uuid = UUIDTools::UUID.random_create.to_s
+
+    if (c.nil?)
+      uuid_address = r.lpop Rails.application.config.redis.uuid_address_list_name 
+      if (uuid_address).nil?
+        #TODO: make this more graceful
+        raise 'out of uuids!'
+      end
+      hash = JSON.parse(uuid_address)
+      r.set hash[:uuid], hash[:address]
       cookies[:uuid] = {
-        :value => uuid,
+        :value => hash[:uuid],
         :expires => 10.years.from_now
       }
-      Resque.enqueue(NewAddressJob, uuid)
+      @address = hash[:address]
+    else
+
+      uuid_address = r.get c
+      if (uuid_address.nil?)
+        #TODO: this could happen from fsync per sec
+      end
+      hash = JSON.parse(uuid_address)
+      @address = hash[:address]
     end
+
   end
 
+
+
+  #state machine:
+  # case 1: user visits index with cookie set and a valid address
+  # case 2: user visits index with cookie set and a nil address
+  # case 3: user visits index with no cookie set 
+  # case 4: user visits show with cookie set and a valid address
+  # case 5: user visits show with cookie set and a nil address
+  # case 6: user visits show with no cookie set and a valid address
+  # case 7: user visits show with no cookie set and a nil address
+  #
+  #
+  # results:
+  # case1: redirect to show so valid will be in url
+  # case2: create a new cookie and rediect so new address will show up
+  # case3: create a new cookie and rediect so address will show up
+  # case4: do nothing special
+  # case5: redirect to show with valid address
+  # case6: set cookie and show
+  # case7: create valid address and cookie, and redirect
   def show
-    uuid = UUIDTools::UUID.raw_string params[:id]
-    if (not uuid.valid?)
-      # do something!
-    end
     r = Redis.new
-    address = r.get uuid
-    if (address.nil?)
-      #do something!
+    c = cookies[:uuid]
+
+    if (not params[:id].nil?)
+      existing_address = r.get params[:id]
+    end
+    if (c.nil? && (not existing_address.nil?))
+      cookies[:uuid] = {
+        :value => params[:id],
+        :expires => 10.years.from_now
+      }
+      @address = existing_address
+    elsif (c.nil?)
+      uuid_address = r.lpop Rails.application.config.redis.uuid_address_list_name 
+      if (uuid_address).nil?
+        #TODO: make this more graceful
+        raise 'out of uuids!'
+      end
+      hash = JSON.parse(uuid_address)
+      r.set hash[:uuid], hash[:address]
+      cookies[:uuid] = {
+        :value => hash[:uuid],
+        :expires => 10.years.from_now
+      }
+      @address = hash[:address]
+    else
+      uuid_address = r.get c
+      if (uuid_address.nil?)
+        #TODO: this could happen from fsync per sec
+      end
+      hash = JSON.parse(uuid_address)
+      @address = hash[:address]
     end
   end
 
