5728f88da5c85a81ce9c5c870faee6ebb00885aa
user <user@users-MacBook-Pro.local>
2013-03-05 22:55:11 -0500

    dont call into your parent!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,16 @@ class HomeController < ApplicationController
   def render_show args
     @data = {}
     @data[:address] = args[:cookie_state]
-    @data[:balance] = BalanceChange.newest_for_user_with_public_id(args[:cookie_id].to_s).pluck('balance').try(:first).try(:to_s)
+    balance = BalanceChange.newest_for_user_with_public_id(args[:cookie_id].to_s).pluck('balance').try(:first)
+    if not balance.nil?
+      @data[:balance_in_dBTC] = (balance * 10).to_i.to_s;
+      @data[:balance_in_cBTC] = (balance * 100).to_i.to_s;
+      @data[:balance_in_mBTC] = (balance * 1000).to_i.to_s;
+    else
+      @data[:balance_in_dBTC] = 0.to_s;
+      @data[:balance_in_cBTC] = 0.to_s;
+      @data[:balance_in_mBTC] = 0.to_s;
+    end
   end
   def get_cookie_state args
     User.where(:public_id => args[:cookie_id]).first.try(:address)
